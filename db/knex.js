054bb816e0fbf4be012eca24403d8d687b8091c2
Heathcliff <dennis.nathan@gmail.com>
2018-11-15 11:58:13 -0700

    Configured environment variables.

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,4 @@
+require('dotenv').config()
 const environment = process.env.NODE_ENV;
-const config = require('../knexfile.js')['development'];
+const config = require('../knexfile.js')[environment];
 module.exports = require('knex')(config);
-
-// console.log(process.env);
