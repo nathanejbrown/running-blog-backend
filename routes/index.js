5954a8abff4f59f1d980716707e262d499aaef38
Heathcliff <dennis.nathan@gmail.com>
2018-11-16 13:06:58 -0700

    Added CORS configuration to start sending requests from front end.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var queries = require('../db/queries.js');
 
 /* GET home page. */
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
